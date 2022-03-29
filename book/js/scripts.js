3c88d612062139afa1b36f60a79342235ad9a31b
sjsmit46 <sjsmit46@asu.edu>
2022-03-29 03:06:53 -0700

    new menu

--- book/js/scripts.js
+++ book/js/scripts.js
@@ -1,9 +1,9 @@
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
   }
