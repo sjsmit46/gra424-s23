ca9d594f6a4be36f05f3a3a7b1094c34421b54f7
sjsmit46 <sjsmit46@asu.edu>
2022-04-24 00:08:47 -0700

    update

--- book/js/scripts.js
+++ book/js/scripts.js
@@ -47,3 +47,12 @@ function showSlides(n) {
 }
 
 
+const [red, green, blue] = [69, 111, 225]
+const section1 = document.querySelector('.section1')
+
+window.addEventListener('scroll', () => {
+  const y = 1 + (window.scrollY || window.pageYOffset) / 150
+  const [r, g, b] = [red/y, green/y, blue/y].map(Math.round)
+  section1.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
+})
+
